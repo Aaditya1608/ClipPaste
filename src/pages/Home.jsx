d633dc5d66493befb63ed05333b7c23e8b0493fb
Aaditya Ashwin Rao <aadityaashwinrao@gmail.com>
2026-06-24 10:00:44 +0530

    Implemented copied text on cards logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,17 @@
 import Card from "../components/Card.jsx";
-import data from '../data.json';
 import Header from "../components/Header.jsx";
 import { useState } from "react";
 import IconButton from "../components/IconButton.jsx";
 import { Copy, X } from "lucide-react";
+import useClipboard from "../hooks/useClipboard.js";
 
 export default function Home() {
   const [selectedItem, setSelectedItem] = useState(null);
+  const {history} = useClipboard();
   return (
-    <div className="h-scroll bg-[#f7f7ff] dark:bg-[#070600] border border-zinc-200 dark:border-zinc-700 overflow-y-auto scrollbar-thin scrollbar-thumb-[#57737a] scrollbar-track-white">
+    <div className="h-screen bg-[#f7f7ff] dark:bg-[#070600] border border-zinc-200 dark:border-zinc-700 overflow-y-auto scrollbar-thin scrollbar-thumb-[#57737a] scrollbar-track-white">
     <Header/>
-    {data.history.map((item) => (
+    {history.map((item) => (
         <Card
           key={item.id}
           item={item}
@@ -18,8 +19,8 @@ export default function Home() {
         />
       ))}
        {selectedItem && (
-        <div className="fixed inset-0 bg-black/50 flex items-center justify-center font-mono">
-          <div className="w-125 max-h-[70vh] rounded-xl bg-[#F0E7D8] dark:bg-[#57737A] p-6 flex flex-col gap-3">
+        <div className="fixed inset-0 bg-black/50 flex items-center justify-center font-mono dark:text-[#f7f7ff]">
+          <div className="w-125 max-h-[70vh] rounded-xl bg-[#F0E7D8] dark:bg-[#57737A] p-6 flex flex-col gap-3 ">
             
               <IconButton Icon={X}  onClick={() => setSelectedItem(null)} size={12}/>
             
@@ -30,7 +31,7 @@ export default function Home() {
                     scrollbar-thumb-rounded-full
                     dark:scrollbar-track-slate-800 
                     dark:scrollbar-thumb-slate-600">
-            <p className="wrap-break-word whitespace-pre-wrap">
+            <p className="wrap-break-word whitespace-pre-wrap border border-black rounded-xl py-2 px-3 dark:bg-black/70 bg-[#f7f7ff]">
               {selectedItem.copiedData}
             </p>
           </div>
